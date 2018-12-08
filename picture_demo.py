9a8444cb0cd43b4b349e140bfbb59e4ecfd57300
louis24 <vortex750@hotmail.com>
2018-12-08 18:13:04 -0500

    'first'

---
+++ picture_demo.py
@@ -0,0 +1,40 @@
+import cv2
+import torch
+import numpy as np
+from network.rtpose_vgg import get_model
+from network.post import decode_pose
+from evaluate.coco_eval import get_multiplier, get_outputs, handle_paf_and_heat
+
+weight_name = './network/weight/pose_model.pth'
+
+model = get_model('vgg19')
+model.load_state_dict(torch.load(weight_name))
+model = torch.nn.DataParallel(model).cuda()
+model.float()
+model.eval()
+
+test_image = './readme/2.jpg'
+oriImg = cv2.imread(test_image)  # B,G,R order
+shape_dst = np.min(oriImg.shape[0:2])
+
+# Get results of original image
+multiplier = get_multiplier(oriImg)
+
+with torch.no_grad():
+    orig_paf, orig_heat = get_outputs(
+        multiplier, oriImg, model, 'rtpose')
+
+    # Get results of flipped image
+    swapped_img = oriImg[:, ::-1, :]
+    flipped_paf, flipped_heat = get_outputs(multiplier, swapped_img,
+                                            model, 'rtpose')
+
+    # compute averaged heatmap and paf
+    paf, heatmap = handle_paf_and_heat(
+        orig_heat, flipped_heat, orig_paf, flipped_paf)
+
+param = {'thre1': 0.1, 'thre2': 0.05, 'thre3': 0.5}
+canvas, to_plot, candidate, subset = decode_pose(
+    oriImg, param, heatmap, paf)
+
+cv2.imwrite('2.png', to_plot)
